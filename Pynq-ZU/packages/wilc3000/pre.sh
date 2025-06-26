9fad4eae610e80557ff15f79595dda52c7dd7a40
Mario Ruiz <mruiznog@amd.com>
2025-06-26 14:21:40 +0100

    More specific way of copying driver

--- Pynq-ZU/packages/wilc3000/pre.sh
+++ Pynq-ZU/packages/wilc3000/pre.sh
@@ -6,11 +6,16 @@
 target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo cp $script_dir/wpa_ap.service $target/lib/systemd/system
-sudo cp -r $script_dir/wpa_ap $target/usr/local/share/
-sudo mkdir $target/wilc_bld
-sudo mkdir -p $target/lib/firmware/mchp
-cd $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project
+sudo cp ${script_dir}/wpa_ap.service ${target}/lib/systemd/system
+sudo cp -r ${script_dir}/wpa_ap ${target}/usr/local/share/
+sudo mkdir ${target}/wilc_bld
+sudo mkdir -p ${target}/lib/firmware/mchp
+cd ${BUILD_ROOT}/${PYNQ_BOARD}/petalinux_project
+
 petalinux-build -c wilc
-sudo mkdir -p ${target}/lib/modules/*/extra
-sudo cp -rf build/tmp/sysroots-components/*/wilc/lib/modules/*/updates/wilc-sdio.ko ${target}/lib/modules/*/extra/
+
+src_dir="build/tmp/sysroots-components/*/wilc/lib/modules/"
+kernel_version=$(ls {src_dir})
+
+sudo mkdir -p ${target}/lib/modules/${kernel_version}/extra
+sudo cp -rf ${src_dir}/*/updates/wilc-sdio.ko ${target}/lib/modules/${kernel_version}/extra/
